c35eebc5d04ed3b20df67d457d6306791f28edcc
Alvagit25 <alvarodesegovia@gmail.com>
2023-12-04 11:15:27 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Header from './components/Header.vue';
+
+</script>
+
+<template>
+<div class="container mx-auto mt-20">
+  <Header/>
+
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
